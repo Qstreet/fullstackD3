baef06038952145d0a22b79b940f49bcf87a233f
Qstreet <git@qstreet.org>
2019-09-19 16:14:33 -0400

    Scripting

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 async function drawLineChart() {
-  // console.log("Hey Chart");
+  // console.log('Hey Chart');
 
-  const dataset = await d3.json("./fullstackD3/my_weather_data.json")
+  const dataset = await d3.json('./fullstackD3/my_weather_data.json')
   // console.log(dataset);
 
   // ACCESSOR fn  convert single data pt into a value
   const yAccessor = d => d.temperatureMax
 
   // TIME PARSE and X ACCESSOR
-  const dateParser = d3.timeParse("%Y-%m-%d")
+  const dateParser = d3.timeParse('%Y-%m-%d')
+  // accessor fn so that xAccessor(dataset[0]) returns first day date
+  // REMEMBER: accessor fns return an unscaled value 
   const xAccessor = d => dateParser(d.date)
 
   // WRAPPER AND BOUNDS
@@ -26,19 +28,56 @@ async function drawLineChart() {
   dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
   dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
 
-  const wrapper = d3.select("#wrapper")
-  const svg = wrapper.append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+  const wrapper = d3.select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
 
   const bounds = wrapper.append('g')
-      .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)` )
-      
-  const yScale = d3.scaleLinear()
-      .domain(d3.extent(dataset, yAccessor))
-      .range([dimensions.boundedHeight, 0])
+    .style('transform', `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
 
-console.log(yScale(32));
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+
+  // FREEZING BLOCK
+  const freezingTemperaturePlacement = yScale(32)
+  const freezingTemperatures = bounds.append('rect')
+    .attr('x', 0)
+    .attr('width', dimensions.boundedWidth)
+    .attr('y', freezingTemperaturePlacement)
+    .attr('height', dimensions.boundedHeight - freezingTemperaturePlacement)
+    .attr('fill', '#e0f3f3')
+
+  const lineGenerator = d3.line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => yScale(yAccessor(d)))
+
+  const line = bounds.append('path')
+    .attr('d', lineGenerator(dataset))
+    .attr('fill', 'none')
+    .attr('stroke', '#af9358')
+    .attr('stroke-width', 2)
+
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+
+  const yAxis = bounds.append('g')
+    .call(yAxisGenerator)
+
+  yAxisGenerator(yAxis)
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+  const xAxis = bounds.append('g')
+    .call(xAxisGenerator)
+    .style('transform', `translateY(${dimensions.boundedHeight}px)`)
 
 }
 
