df9e9640a4a97d56dd7b82ec03a5eef2ac144c9f
Qstreet <git@qstreet.org>
2019-10-04 06:12:35 -0400

    Update

--- GraphGalleryHisto/script.js
+++ GraphGalleryHisto/script.js
@@ -1,3 +1,7 @@
+// src
+//  https://www.d3-graph-gallery.com/graph/histogram_binSize.html
+
+
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 30, left: 40 },
   width = 460 - margin.left - margin.right,
